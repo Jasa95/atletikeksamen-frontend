8355e05feea6be9f0f8cb288531aac6624bd2aab
Jasa95 <jaco3020@stud.kea.dk>
2024-06-20 21:51:15 +0200

    ??

--- src/services/apiFacade.ts
+++ src/services/apiFacade.ts
@@ -1,22 +1,21 @@
 // Here you are supposed to insert your fetch methods. Remember to match them to your backend endpoints.
-import { handleHttpErrors,  } from "./fetchUtils";
+import { handleHttpErrors } from "./fetchUtils";
 
 const LOCALHOST = "http://localhost:8080";
 const PARTICIPANTS_URL = LOCALHOST + "/api/participants";
 const RESULTS_URL = LOCALHOST + "/api/results";
 const DISCIPLINES_URL = LOCALHOST + "/api/discipline";
 
-
 async function getAllParticipants() {
-    return fetch(PARTICIPANTS_URL).then(handleHttpErrors);
+  return fetch(PARTICIPANTS_URL).then(handleHttpErrors);
 }
 
 async function getAllResults() {
-    return fetch(RESULTS_URL).then(handleHttpErrors);
+  return fetch(RESULTS_URL).then(handleHttpErrors);
 }
 
 async function getAllDisciplines() {
-    return fetch(DISCIPLINES_URL).then(handleHttpErrors);
+  return fetch(DISCIPLINES_URL).then(handleHttpErrors);
 }
 /*
 //Replace data with the parameter you want to send just edit the interface and the parameter from entityFacade
@@ -37,8 +36,4 @@ async function deleteTestData(id: number) {
     return fetch(TEST_URL + "/" + id, options).then(handleHttpErrors);
 }
 */
-export {
-    getAllParticipants,
-    getAllResults,
-    getAllDisciplines
-}
+export { getAllParticipants, getAllResults, getAllDisciplines };
